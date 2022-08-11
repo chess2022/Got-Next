1e6a7923a76735400e79a3305b9f07c584b6108c
chess2022 <lakeshore.hnh@gmail.com>
2022-08-10 18:55:09 -0700

    Update the header for chat index screen

--- app/screens/Chat/ChatHomeScreen.jsx
+++ app/screens/Chat/ChatHomeScreen.jsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useEffect, useState, useLayoutEffect } from 'react'
-import { StyleSheet, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import { StyleSheet, SafeAreaView, ScrollView, TouchableOpacity, View, Text } from 'react-native'
 import CustomListItem from '../components/CustomListItem'
 import { collection, getFirestore, onSnapshot } from '../../config/firebase'
-import { SimpleLineIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 
 function ChatHomeScreen({navigation}) {
@@ -15,19 +15,24 @@ function ChatHomeScreen({navigation}) {
       onSnapshot(collection(db, 'chats'), (snapshot) => {
         setChats(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })))
       }),[])
-      
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Chat Topics",
-      headerStyle: { backgroundColor: 'white' },
+      headerStyle: { backgroundColor: "white" },
       headerTitleStyle: { color: "black" },
       headerTintColor: "black",
       headerRight: () => (
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => navigation.navigate("AddChat")}>
-            <SimpleLineIcons name="pencil" size={18} color="black" />
-        </TouchableOpacity>
+        <View style={{flexDirection:"row", alignItems:"center", marginLeft: 10}}>
+          <Text>Start a new chat</Text>
+          <TouchableOpacity
+            style={{marginLeft: 10}}
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("AddChat")}
+          >
+            <Ionicons name="ios-add-circle-sharp" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
       ),
     });
   }, [navigation]);
